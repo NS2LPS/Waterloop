278b7f934fc18888e6e30d7724cee374719375c8
Jérôme Esteve <jerome.esteve@universite-paris-saclay.fr>
2026-04-30 07:28:33 +0200

    Removed unused plots

--- sensors.py
+++ sensors.py
@@ -36,9 +36,9 @@ class FloatSensorValidRange(FloatSensor):
         self.min_value = min
         self.max_value = max
     def validate(self, value: float) -> int:
-        if value < self.min_value:
+        if self.min_value is not None and value < self.min_value:
             return 1
-        if value > self.max_value:
+        if self.max_value is not None and value > self.max_value:
             return 2
         return 0
     def alarm_msg(self, errcode, language):
@@ -80,78 +80,3 @@ class IntSensorValidValues(IntSensor):
             return f"{d} : {translate('invalid_state',language)}"
     
 
-
-# Primary circcuit (eau glacée)
-primary_pressure_1 = FloatSensorValidRange(
-    {"en": "Primary Circuit Pressure", "fr": "Pression primaire"},
-    "bar",
-    2.0,3.5,
-    )
-
-primary_temperature_1 = FloatSensorValidRange(
-    {"en": "Primary Temperature", "fr": "Température primaire"},
-    "°C",
-    5.0,15.0,
-    )
-
-primary_temperature_2 = FloatSensor(
-    {"en": "Primary Temperature (back)", "fr": "Température primaire (retour)"},
-    "°C",
-    )
-
-valve_command = FloatSensor(
-    {"en": "Mixing Valve", "fr": "Vanne échangeur"},
-    "%",
-    )
-
-pmp07_state = IntSensorValidValues(
-    {"en": "Primary Pump State", "fr": "Pompe circuit primaire"},
-    [3,],
-    )
-
-# Groupes froids
-gf01_state = StateSensor({"en":"GF01 State","fr":"GF01"})
-gf01_temperature_out = FloatSensor(
-    {"en": "GF01 Exit Temperature", "fr": "GF01 sortie"},
-    "°C",
-    )
-
-gf02_state = StateSensor({"en":"GF02 State","fr":"GF02"})
-gf02_temperature_out = FloatSensor(
-    {"en": "GF02 Exit Temperature", "fr": "GF02 sortie"},
-    "°C",
-    )
-
-
-# Secondary circuit (boucle d'eau)
-secondary_temperature_1 = FloatSensorValidRange(
-    {"en": "Water Loop Temperature", "fr": "Température boucle d'eau"},
-    "°C",
-    13.5,20.5,
-    )
-
-secondary_temperature_2 = FloatSensor(
-    {"en": "Water Loop Temperature (SEMFEG)", "fr": "Température boucle d'eau (SEMFEG)"},
-    "°C",
-    )
-
-secondary_flow_1 = FloatSensorValidRange(
-    {"en": "Water Loop Flow", "fr": "Débit boucle d'eau"},
-    "L/min",
-    0,1000,
-    )
-
-SIGNAL_TABLE = {
-    "primary_pressure_1": primary_pressure_1,
-    "primary_temperature_1": primary_temperature_1,
-    "primary_temperature_2": primary_temperature_2,
-    "pmp07_state" : pmp07_state,
-    "secondary_flow_1": secondary_flow_1,
-    "secondary_temperature_1": secondary_temperature_1,
-    "secondary_temperature_2": secondary_temperature_2,
-    "gf01_temperature_out": gf01_temperature_out,
-    "gf02_temperature_out": gf02_temperature_out,
-    "gf01_state": gf01_state,
-    "gf02_state": gf02_state,
-    "valve_command": valve_command
-}
