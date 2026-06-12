0f2220cbe7295370688f6b30685096a6413e4bb8
Jérôme Esteve <jerome.esteve@universite-paris-saclay.fr>
2026-06-12 18:44:29 +0200

    Remove upper limit for presseure

--- signals.py
+++ signals.py
@@ -5,7 +5,7 @@ import sensors
 primary_pressure_1 = sensors.FloatSensorValidRange(
     {"en": "Primary Circuit Pressure", "fr": "Pression primaire"},
     "bar",
-    1.5,3.5,
+    1.5,None,
     )
 
 primary_temperature_1 = sensors.FloatSensorValidRange(
